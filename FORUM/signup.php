5c36dae85dfb01f66b7ad752703cf719e82b26a3
fastoch <fastoch@ik.me>
2021-11-13 12:08:20 +0100

    forum php sql

---
+++ FORUM/signup.php
@@ -0,0 +1,9 @@
+<!--Pour générer un template html depuis un fichier .php, taper ! puis Entrée. 
+VS Code génère automatiquement le template html.-->
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    
+</body>
+</html>
