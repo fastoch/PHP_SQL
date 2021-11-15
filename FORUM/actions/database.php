932dceb654c5b25257be61b44ed16508b13ce92a
fastoch <fastoch@ik.me>
2021-11-15 22:03:07 +0100

    *

--- FORUM/actions/database.php
+++ FORUM/actions/database.php
@@ -1,10 +1,10 @@
 <?php
 
 // on crée une variable qui va nous permettre d'accéder à notre database
-$db = new PDO('') // $db est une nouvelle instance de la classe PDO*
+$db = new PDO('mysql:host = localhost ; dbname = forum ; charset = utf8', 'root', ''); // nouvelle instance de la classe PDO*
+// ici, nous avons spécifié le type de DB, l'hôte = localhost, le nom de la DB, le jeu de caractères utilisé, puis enfin le login et le mot de passe pour accéder à la DB.
 
 ?>
 
-*PHP Data Objects est une extension définissant l'interface pour accéder à une base de données avec PHP. 
-Elle est orientée objet, la classe s’appelant PDO. 
+*PHP Data Objects est une extension définissant l'interface pour accéder à une base de données avec PHP. Elle est orientée objet, la classe s’appelant PDO. 
 PDO constitue une couche d'abstraction qui intervient entre l'application PHP et un système de gestion de base de données tel que MySQL ou PostgreSQL.
